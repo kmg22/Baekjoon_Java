854406b4287617af1d01a63f929538d5bfad7887
kmg22 <ssukmg22@gmail.com>
2023-08-14 23:00:44 +0900

    baekjoon

--- src/baekjoon_2164.java
+++ src/baekjoon_2164.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Main {
+public class baekjoon_2164 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -24,3 +24,15 @@ public class Main {
         System.out.println(cardset.poll());
     }
 }
+/*
+1 2 3 4 5 6
+  2 3 4 5 6 >> 1
+  3 4 5 6 2
+    4 5 6 2 >> 3
+    5 6 2 4
+      6 2 4 >> 5
+      2 4 6
+        4 6 >> 2
+        6 4
+          4 >> 6
+ */
